4c7e6fd50c7d3a11d7dbe2bd51e98d624219bce3
tomy08 <tomasesantacruz@gmail.com>
2024-10-01 10:46:02 -0300

    Refactor table component: Remove unused import and code

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -2,8 +2,6 @@ import * as React from 'react'
 
 import { cn } from '@/lib/utils'
 
-import { redirect } from 'next/navigation'
-
 const Table = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement>
