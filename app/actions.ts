c667ba6672f9357d08fa53963aed6a1ccd609b98
tomy08 <tomasesantacruz@gmail.com>
2024-10-02 10:06:22 -0300

    Refactor actions.ts: Add friend-related functions ✨

--- app/actions.ts
+++ app/actions.ts
@@ -182,3 +182,71 @@ export const updateUserProfile = async (formData: FormData) => {
   redirect(`/player/${userId}`)
   return { data }
 }
+
+const sendFriendRequest = async (friendEmail: string) => {
+  const supabase = createClient()
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  const { data: friend, error } = await supabase
+    .from('user')
+    .select('*')
+    .eq('email', friendEmail)
+    .single()
+
+  if (friend) {
+    const { error } = await supabase
+      .from('friends')
+      .insert({ user_id: user?.id, friend_id: friend.id, status: 'pending' })
+
+    if (error) console.error(error)
+    else alert('Solicitud enviada')
+  }
+}
+
+const acceptFriendRequest = async (friendId: string) => {
+  const supabase = createClient()
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  const { error } = await supabase
+    .from('friends')
+    .update({ status: 'accepted' })
+    .eq('user_id', friendId)
+    .eq('friend_id', user?.id)
+
+  if (error) console.error(error)
+  else alert('Solicitud aceptada')
+}
+
+// Rechazar solicitud
+const rejectFriendRequest = async (friendId: string) => {
+  const supabase = createClient()
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  const { error } = await supabase
+    .from('friends')
+    .delete()
+    .eq('user_id', friendId)
+    .eq('friend_id', user?.id)
+
+  if (error) console.error(error)
+  else alert('Solicitud rechazada')
+}
+
+const getFriends = async () => {
+  const supabase = createClient()
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+  const { data, error } = await supabase
+    .from('friends')
+    .select('*')
+    .or(`user_id.eq.${user?.id}, friend_id.eq.${user?.id}`)
+
+  return { data, error }
+}
