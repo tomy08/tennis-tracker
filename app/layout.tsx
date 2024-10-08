27c0a67f72831f4d9fac614473ca2118c5a36851
tomy08 <tomasesantacruz@gmail.com>
2024-10-07 21:39:52 -0300

    Refactor layout.tsx: Add title to HTML head

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={GeistSans.className} suppressHydrationWarning>
+      <head>
+        <title>Tennis Tracker</title>
+      </head>
       <body className="bg-background text-foreground">
         <ThemeProvider
           attribute="class"
@@ -33,37 +36,22 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <main className="min-h-screen flex flex-col items-center">
-            <div className="flex-1 w-full flex flex-col gap-20 items-center">
-              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-                <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                  <div className="flex gap-5 items-center font-semibold">
-                    <Link href={'/'}>Next.js Supabase Starter</Link>
-                    <div className="flex items-center gap-2">
-                      <DeployButton />
-                    </div>
-                  </div>
-                  {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
-                </div>
-              </nav>
-              <div className="flex flex-col gap-20 max-w-5xl p-5">
-                {children}
-              </div>
+            <div className="flex flex-col max-w-5xl p-5">{children}</div>
 
-              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-                <div>
-                  Tennis Tracker was a project made by{' '}
-                  <a
-                    href="https://github.com/tomy08"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="underline"
-                  >
-                    Tomas Santa Cruz
-                  </a>
-                </div>
-                <ThemeSwitcher />
-              </footer>
-            </div>
+            <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+              <div>
+                Tennis Tracker was a project made by{' '}
+                <a
+                  href="https://github.com/tomy08"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="underline"
+                >
+                  Tomas Santa Cruz
+                </a>
+              </div>
+              <ThemeSwitcher />
+            </footer>
           </main>
         </ThemeProvider>
       </body>
