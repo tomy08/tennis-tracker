3476ba32dc20544c0375bdc30ada6a07f99890a4
tomy08 <tomasesantacruz@gmail.com>
2024-10-23 09:56:04 -0300

    Update metadata in layout.tsx and package-lock.json

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: 'Next.js and Supabase Starter Kit',
-  description: 'The fastest way to build apps with Next.js and Supabase',
+  title: 'Tennis Tracker',
+  description: 'Track your tennis matches and improve your game.',
 }
 
 export default function RootLayout({
