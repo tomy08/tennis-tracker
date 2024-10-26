a333a2909db530a0869fcb163679772cc624fc6b
tomy08 <tomasesantacruz@gmail.com>
2024-10-26 10:10:13 -0300

    Refactor main-page.tsx: Add background to main element

--- components/main-page.tsx
+++ components/main-page.tsx
@@ -13,7 +13,7 @@ export function MainPage() {
     window.location.href = `/find-players/${location}`
   }
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-background">
       <main className="flex-1">
         <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
           <div className="container px-4 md:px-6">
@@ -39,7 +39,7 @@ export function MainPage() {
           </div>
         </section>
 
-        <section className="py-12 md:py-24 lg:py-32 bg-gray-100 dark:bg-black/10 w-full">
+        <section className="py-12 md:py-24 lg:py-32  w-full">
           <div className="container px-4 md:px-6">
             <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl text-center mb-8">
               Features
